f62580bd511014b2f8fdfb5492d25f6dc7d9feb1
aishyuu <ivanmatias1331@gmail.com>
2024-02-15 20:58:02 -0800

    Added github source link to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import logo from "../images/weather_icon.png";
+import github from "../images/github.png";
 
 export default function header() {
   const headerDiv = document.querySelector(".header");
@@ -6,10 +7,17 @@ export default function header() {
   const headerImage = document.createElement("img");
   headerImage.src = logo;
   headerImage.classList.add("header-image");
-
   headerDiv.appendChild(headerImage);
 
-  const sourceLink = 
+  const sourceLink = document.createElement("a");
+  sourceLink.classList.add("header-github-div");
+  sourceLink.href = "https://github.com/aishyuu/weather-app-v2"
+  sourceLink.target = "_blank"
+  
+  const sourceImage = document.createElement("img");
+  sourceImage.src = github;
+  sourceImage.classList.add("header-source-image")
+  sourceLink.appendChild(sourceImage);
 
-  headerDiv.appendChild(headerTempButton);
+  headerDiv.appendChild(sourceLink);
 }
