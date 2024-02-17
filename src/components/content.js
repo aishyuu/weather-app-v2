218ba542f0887833e563dc30c2ae5d871dd37e69
aishyuu <ivanmatias1331@gmail.com>
2024-02-16 21:30:02 -0800

    Added name of city to current weather

--- src/components/content.js
+++ src/components/content.js
@@ -38,11 +38,34 @@ function displayInformation(data) {
 function displayCurrentWeather(data) {
   const contentDiv = document.querySelector(".content");
   const currentData = data.current;
+  const currentWeatherAllDiv = document.createElement("div");
+  currentWeatherAllDiv.classList.add("weather-current");
+
+  const currentWeatherBigInfo = document.createElement("div");
+  currentWeatherBigInfo.classList.add("weather-current-big");
+
+  const currentWeatherCity = document.createElement("p");
+  currentWeatherCity.classList.add("weather-current-city");
+  currentWeatherCity.textContent = `${data.location.name}, ${data.location.country}`;
+  currentWeatherBigInfo.appendChild(currentWeatherCity);
+
+  const currentWeatherImgTempDiv = document.createElement("div");
+  currentWeatherImgTempDiv.classList.add("weather-current-imgtemp");
 
   const weatherImage = document.createElement("img");
+  weatherImage.classList.add("weather-current-img");
   weatherImage.src = `https:${currentData.condition.icon}`;
+  currentWeatherImgTempDiv.appendChild(weatherImage);
 
-  contentDiv.appendChild(weatherImage)
+  const weatherTemp = document.createElement("h1");
+  weatherTemp.classList.add("weather-current-temp");
+  weatherTemp.textContent = `°${currentData.temp_f}`;
+  currentWeatherImgTempDiv.appendChild(weatherTemp);
+
+  currentWeatherBigInfo.appendChild(currentWeatherImgTempDiv);
+
+  currentWeatherAllDiv.appendChild(currentWeatherBigInfo);
+  contentDiv.appendChild(currentWeatherAllDiv);
 }
 
 export default function content() {
