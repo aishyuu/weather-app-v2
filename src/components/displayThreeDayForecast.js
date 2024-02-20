f0d65a346b75fb9f1fb8f4ad849d3dd55690a05b
aishyuu <ivanmatias1331@gmail.com>
2024-02-20 01:57:50 -0800

    Completed 3 day forecast

--- src/components/displayThreeDayForecast.js
+++ src/components/displayThreeDayForecast.js
@@ -10,11 +10,31 @@ export default function displayThreeDayForecast(data, isCel) {
     const element = forecastData[index];
     const dailyDiv = document.createElement("div");
     dailyDiv.classList.add("day-forecast");
+    console.log(element);
 
     const dailyDate = document.createElement("p");
     dailyDate.classList.add("day-date");
     dailyDate.textContent = moment.unix(element.date_epoch).format("MMM D");
     dailyDiv.appendChild(dailyDate);
+
+    const dailyImage = document.createElement("img");
+    dailyImage.classList.add("day-img");
+    dailyImage.src = `https:${element.day.condition.icon}`;
+    dailyDiv.appendChild(dailyImage);
+
+    const dailyTemp = document.createElement("p");
+    dailyTemp.classList.add("day-temp");
+    if (isCel) {
+        dailyTemp.textContent = `${element.day.avgtemp_c}`
+    } else {
+        dailyTemp.textContent = `${element.day.avgtemp_f}°`
+    }
+    dailyDiv.appendChild(dailyTemp);
+    
+    const dailyRainChance = document.createElement("p");
+    dailyRainChance.classList.add("day-rain");
+    dailyRainChance.textContent = `Rain %: ${element.day.daily_chance_of_rain}`
+    dailyDiv.appendChild(dailyRainChance)
 
     forecastDiv.appendChild(dailyDiv);
   }
